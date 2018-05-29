27339d4c5d1baef3c9c8fa300f8ab0fdf75c64d4
Dao Truong Dao <Daoprovip2014@gmail.com>
2018-05-29 23:18:00 +0700

    tao canh cua va tao vat pham

---
+++ Assets/Scripts/Collectables Script/DiamondScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (Door.instance != null)
+        {
+            Door.instance.collecttablesCount++;
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Player")
+        {
+            Destroy(gameObject);
+            if (Door.instance != null) // luôn luôn phải có hàm Door
+            {
+                Door.instance.DecrementCollectables();//nhân vật ăn 1 viên sẽ giảm 1 đơn vị
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Door Script/Door.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    public static Door instance;// hàm tĩnh có thể sử dụng biến này và truy xuất
+
+    private Animator anim;
+    private BoxCollider2D box;
+
+    [HideInInspector]
+    public int collecttablesCount;
+
+    void Awake()
+    {
+        MakeInstance();
+        anim = GetComponent<Animator>();
+        box = GetComponent<BoxCollider2D>();
+    }
+
+    void MakeInstance()
+    {
+        if (instance == null)
+        {
+            instance = this; // trỏ tới claas gần nhất là Door
+        }
+    }
+    // ăn được các item thì nó sẽ giảm xuống 0 thì cửa sẽ mở
+    public void DecrementCollectables()
+    {
+        collecttablesCount--;
+        if (collecttablesCount == 0)
+        {
+            StartCoroutine(OpenDoor());
+        }
+    }
+
+    IEnumerator OpenDoor()
+    {
+        anim.Play("Open");
+        yield return new WaitForSeconds(.7f);
+        box.isTrigger = true;
+    }
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Player")
+        {
+            Debug.Log("Finish");
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
